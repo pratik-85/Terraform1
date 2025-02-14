e81277fad9472d7664df3b0ccc74192d563e7ca3
pratik-85 <94506839+pratik-85@users.noreply.github.com>
2025-02-14 14:24:35 +0530

    Create main.tf
    EKS Cluster

---
+++ main.tf
@@ -0,0 +1,143 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Fetch the Default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Fetch the Default Subnets (Excluding unsupported zones)
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  }
+}
+
+# Create IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach EKS Policies to IAM Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+# Create IAM Role for Worker Nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach Policies to Worker Node IAM Role
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "cbz_cluster" {
+  name     = "cbz-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_service_policy
+  ]
+}
+
+# Create EKS Node Group
+resource "aws_eks_node_group" "cbz_nodegroup" {
+  cluster_name    = aws_eks_cluster.cbz_cluster.name
+  node_group_name = "cbz-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_policy
+  ]
+}
+
+# Automatically Update Kubeconfig for kubectl Access
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks --region ap-southeast-2 update-kubeconfig --name cbz-cluster"
+  }
+
+  depends_on = [aws_eks_cluster.cbz_cluster]
+}
+
+# Output the EKS Cluster Details
+output "eks_cluster_name" {
+  value = aws_eks_cluster.cbz_cluster.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.cbz_cluster.endpoint
+}
+
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.cbz_cluster.arn
+}
